2ab1adacaa05e1b513a954f0e0231665eb5d3840
Lars-B <berlinglars96@gmail.com>
2025-05-23 10:54:37 -0700

    Reformat and removal of redundant variable, CompEvol/BeastFX#99

--- src/beastfx/app/treeannotator/services/MCCTopologyService.java
+++ src/beastfx/app/treeannotator/services/MCCTopologyService.java
@@ -1,29 +1,29 @@
 package beastfx.app.treeannotator.services;
 
 
-import java.io.IOException;
-import java.io.PrintStream;
-
 import beast.base.evolution.tree.Tree;
 import beastfx.app.treeannotator.CladeSystem;
 import beastfx.app.treeannotator.TreeAnnotator;
 import beastfx.app.treeannotator.TreeAnnotator.TreeSet;
 
+import java.io.IOException;
+import java.io.PrintStream;
+
 public class MCCTopologyService implements TopologySettingService {
 
-	
-	protected TreeSet treeSet;
-	protected int totalTreesUsed;
-	
-	@Override
-	public Tree setTopology(TreeSet treeSet, PrintStream progressStream, TreeAnnotator annotator) throws IOException {
-        progressStream.println("Finding maximum credibility tree...");
-		this.treeSet = treeSet;
-		this.totalTreesUsed = annotator.getTotalTreesUsed();
-		return summarizeTrees(annotator.getCladeSystem(), false, progressStream);
-	}
 
-    protected Tree summarizeTrees(CladeSystem cladeSystem, boolean useSumCladeCredibility, PrintStream progressStream) throws IOException  {
+    protected TreeSet treeSet;
+    protected int totalTreesUsed;
+
+    @Override
+    public Tree setTopology(TreeSet treeSet, PrintStream progressStream, TreeAnnotator annotator) throws IOException {
+        progressStream.println("Finding maximum credibility tree...");
+        this.treeSet = treeSet;
+        this.totalTreesUsed = annotator.getTotalTreesUsed();
+        return summarizeTrees(annotator.getCladeSystem(), false, progressStream);
+    }
+
+    protected Tree summarizeTrees(CladeSystem cladeSystem, boolean useSumCladeCredibility, PrintStream progressStream) throws IOException {
 
         Tree bestTree = null;
         double bestScore = Double.NEGATIVE_INFINITY;
@@ -32,24 +32,23 @@ public class MCCTopologyService implements TopologySettingService {
         progressStream.println("0              25             50             75            100");
         progressStream.println("|--------------|--------------|--------------|--------------|");
 
-        int stepSize = Math.max(totalTreesUsed / 60, 1);
         int reported = 0;
 
         int counter = 0;
         treeSet.reset();
         while (treeSet.hasNext()) {
-        	Tree tree = treeSet.next();
+            Tree tree = treeSet.next();
             double score = scoreTree(tree, cladeSystem, useSumCladeCredibility);
-          if (score > bestScore) {
-              bestTree = tree;
-              bestScore = score;
-          }
-		  while (reported < 61 && 1000.0 * reported < 61000.0 * (counter + 1) / totalTreesUsed) {
-              progressStream.print("*");
-              reported++;
-              progressStream.flush();
-    	  }
-          counter++;
+            if (score > bestScore) {
+                bestTree = tree;
+                bestScore = score;
+            }
+            while (reported < 61 && 1000.0 * reported < 61000.0 * (counter + 1) / totalTreesUsed) {
+                progressStream.print("*");
+                reported++;
+                progressStream.flush();
+            }
+            counter++;
         }
         progressStream.println();
         progressStream.println();
@@ -71,13 +70,13 @@ public class MCCTopologyService implements TopologySettingService {
         }
     }
 
-	@Override
-	public String getServiceName() {
-		return "MCC";
-	}
+    @Override
+    public String getServiceName() {
+        return "MCC";
+    }
 
-	@Override
-	public String getDescription() {
-		return "Maximum clade credibility tree";
-	}
+    @Override
+    public String getDescription() {
+        return "Maximum clade credibility tree";
+    }
 }
